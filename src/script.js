0f5e8b727160847b0321116d1b1f9af82f379df0
ciarasugrue <ciarasugrue@gmail.com>
2021-07-11 11:40:07 +0100

    Added forecast API

--- src/script.js
+++ src/script.js
@@ -29,7 +29,7 @@ function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tues"];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -56,6 +56,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "eb6eea518ef4b4002fbbf1c966d1b147";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   celsiusTemp = response.data.main.temp;
 
@@ -79,6 +85,8 @@ function showTemperature(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(event) {
